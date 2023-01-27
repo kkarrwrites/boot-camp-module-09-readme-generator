5214da52e2216e5198ab2569df1b534fefa89df3
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-01-27 16:33:00 -0500

    Create function to return license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -34,7 +34,37 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "None") {
+    return `N/A`;
+  } else if (license === "Apache License 2.0") {
+    return `[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license === "GNU General Public License v3.0") {
+    return `[GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (license === "MIT License") {
+    return `[MIT License](https://opensource.org/licenses/MIT)`;
+  } else if (license === "BSD 2-Clause 'Simplified' License") {
+    return `[BSD 2-Clause 'Simplified' License](https://opensource.org/licenses/BSD-2-Clause)`;
+  } else if (license === "BSD 3-Clause 'New' or 'Revised' License") {
+    return `[BSD 3-Clause 'New' or 'Revised' License](https://opensource.org/licenses/BSD-3-Clause)`;
+  } else if (license === "Boost Software License 1.0") {
+    return `[Boost Software License 1.0](https://www.boost.org/LICENSE_1_0.txt)`;
+  } else if (license === "Creative Commons Zero v1.0 Universal") {
+    return `[Creative Commons Zero v1.0 Universal](http://creativecommons.org/publicdomain/zero/1.0/)`;
+  } else if (license === "Eclipse Public License 2.0") {
+    return `[Eclipse Public License 2.0](https://opensource.org/licenses/EPL-1.0)`;
+  } else if (license === "GNU Affero General Public License v3.0") {
+    return `[GNU Affero General Public License v3.0](https://www.gnu.org/licenses/agpl-3.0)`;
+  } else if (license === "GNU General Public License v3.0") {
+    return `[GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (license === "GNU Lesser General Public License v3.0") {
+    return `[GNU Lesser General Public License v3.0](https://www.gnu.org/licenses/lgpl-3.0)`;
+  } else if (license === "Mozilla Public License 2.0") {
+    return `[Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)`;
+  } else if (license === "The Unlicense") {
+    return `[The Unlicense](http://unlicense.org/)`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
